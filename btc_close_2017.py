005efe49273d398c799581a70326c3d90b91251d
RichardlauCx <2409608770@qq.com>
2019-12-17 16:19:58 +0800

    Integrate the five previously drawn images into the data dashboard

--- btc_close_2017.py
+++ btc_close_2017.py
@@ -115,7 +115,8 @@ def dash_board():
             '收盘价折线图（￥）.svg', '收盘价对数变换折线图.svg', '收盘价月日均值（￥）.svg',
             '收盘价周日均值（￥）.svg', '收盘价星期均值（￥）.svg'
         ]:
-            html_file.write('    <object type="image/svg+xml" data="{0}" height=500></object>\n')
+            html_file.write('    <object type="image/svg+xml" data="{0}" height=500></object>\n'.format(svg))
+        html_file.write('</body></html>')
 
 
 if __name__ == '__main__':
@@ -131,10 +132,12 @@ if __name__ == '__main__':
     # idx_week = date.index('2017-12-11')
     # line_chart_week = draw_line(week[1:idx_week], close[1:idx_week], '收盘价周日均值（￥）', '周日均值')
 
-    idx_week = date.index('2017-12-11')
-    wd = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']  # 相当于字典功能
-    weekdays_int = [wd.index(w) + 1 for w in weekday[1: idx_week]]
-    line_chart_weekday = draw_line(weekdays_int, close[1:idx_week], '收盘价星期均值（￥）', '星期均值')
-    # x轴默认会根据数据，从小到大排序
-    line_chart_weekday.x_labels = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-    line_chart_weekday.render_to_file('收盘价星期均值（￥）.svg')
+    # idx_week = date.index('2017-12-11')
+    # wd = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']  # 相当于字典功能
+    # weekdays_int = [wd.index(w) + 1 for w in weekday[1: idx_week]]
+    # line_chart_weekday = draw_line(weekdays_int, close[1:idx_week], '收盘价星期均值（￥）', '星期均值')
+    # # x轴默认会根据数据，从小到大排序
+    # line_chart_weekday.x_labels = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    # line_chart_weekday.render_to_file('收盘价星期均值（￥）.svg')
+
+    # dash_board()
